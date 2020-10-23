56af32e9bc563ad46ab2c338636bd5b178fb9fa5
liyichao <1120805364@qq.com>
2020-10-23 18:59:45 +0800

    inital

---
+++ src/pages/home/index.js
@@ -0,0 +1,4 @@
+import '../../assets/styles/index.scss';
+import './index.scss';
+
+console.log('首页');
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const { join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+// console.log('node env', process.env.NODE_ENV);
+
+const meta = {
+  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+}
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    home: './src/pages/home/index.js',
+    singers: './src/pages/singers/index.js',
+  },
+  output: {
+    // context: __dirname,
+    path: join(__dirname, 'dist'),
+    filename: "[name]/[name].js"
+  },
+  devServer: {
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "首页",
+      template: './src/pages/home/index.html',
+      filename: "home/index.html",
+      chunks: ['home'],
+      meta
+    }),
+    new HtmlWebpackPlugin({
+      title: "歌手列表",
+      template: './src/pages/singers/index.html',
+      filename: "singers/index.html",
+      chunks: ['singers'],
+      meta
+    }),
+  ]
+}
