268d9a45b6dcffb22594a7ab70da81f352f44af8
lycHub <1120805364@qq.com>
2020-10-24 12:18:11 +0800

    rem ok

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // console.log('node env', process.env.NODE_ENV);
 
 const meta = {
-  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+  'apple-mobile-web-app-capable': 'yes',
+  viewport: 'width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover',
+  'apple-mobile-web-app-status-bar-style': 'block',
+  'format-detection': 'telephone=no',
+  'wap-font-scale': 'no',
+  'Content-Security-Policy': { 'http-equiv': 'Content-Type', 'content': 'text/html; charset=utf-8' }
 }
 
 
@@ -27,7 +33,7 @@ module.exports = {
     }
   },
   devServer: {
-
+    port: 4200
   },
   module: {
     rules: [
@@ -38,11 +44,17 @@ module.exports = {
       },
       {
         test: /\.(scss|sass)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'px2rem-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
+    // new CopyWebpackPlugin({
+    //   patterns: [{
+    //     from: './public',
+    //     to: join(__dirname, 'dist/public', '[name].[ext]')
+    //   }]
+    // }),
     new HtmlWebpackPlugin({
       title: "首页",
       template: './src/pages/home/index.html',
